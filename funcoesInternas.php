0cb1f2fb1a729275cfc7c8e13c8dc03914f547bb
rsouza13 <tiagorsouza13@gmail.com>
2021-09-11 09:46:19 -0300

    corrigindo dados do contato em funcoesInternas.php

--- funcoesInternas.php
+++ funcoesInternas.php
@@ -3,7 +3,7 @@ require('setup.php');
 
 $smarty = new Smarty_GuestBook;
 
-$smarty->assign("contacts", array(array("nome"=>"José","phone" => "1", "fax" => "2", "cell" => "3"),
+$smarty->assign("contacts", array(array("nome"=>"José","phone" => "123-123", "fax" => "22-22", "cell" => "99999-9999"),
       array("nome"=>"Maria","phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
 
 $smarty->display('funcoesInternas.tpl');
